20ea1786d998b1bc9f010ac22ccc9a7e42bad712
Stefan Sundin <git@stefansundin.com>
2022-06-19 21:38:54 -0700

    Add service worker to enable offline support.

---
+++ README.md
@@ -0,0 +1,3 @@
+Icon from:
+- https://openclipart.org/detail/315101/qr-code-silhouette-by-wir-sind-klein
+- https://pixabay.com/photos/qr-code-barcode-miniature-figures-3970681/
---
+++ sw.js
@@ -0,0 +1,23 @@
+// Increment this number to trigger offline clients to update their caches:
+// v1
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+
+  e.waitUntil(
+    caches
+      .open('2fa-qr')
+      .then(c =>
+        c.addAll([
+          'https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css',
+          'https://cdn.jsdelivr.net/npm/kjua@0.9.0/dist/kjua.min.js',
+          'https://cdn.jsdelivr.net/gh/stefansundin/qr-detector.js@v0.0.3/dist/QrDetector.min.js',
+          'https://fonts.googleapis.com/css?family=Roboto:500,400',
+        ]),
+      ),
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
